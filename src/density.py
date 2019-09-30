c352af00c002ca72c27a1e65cd1b3d5eaa82dc9d
birdalugureren <birdalugureren@gmail.com>
2019-10-01 00:26:47 +0300

    edit pivot sum

--- src/density.py
+++ src/density.py
@@ -9,8 +9,8 @@ class Density:
         self.density_data = self._calc_density()
 
     def _calc_density(self):
-        df = self.data.groupby(self.data.columns.tolist()).size(
-        ).reset_index().rename(columns={0: 'density'})
+        df = self.data.groupby(self.data.columns.tolist()).size()\
+            .reset_index().rename(columns={0: 'density'})
         df['density'] = df['density']/df['density'].sum()
         return df
 
