ab160bc53b9d7fffbaedf3da58c2bc6d5fa3905f
birdalugureren <birdalugureren@gmail.com>
2019-11-16 14:38:19 +0300

    new design of _condition

--- src/_condition.py
+++ src/_condition.py
@@ -1,24 +1,52 @@
+import pandas as pd
+
+
+# # Taslak
 class Condition_Types:
 
-    def __init__(self,type,*arg):
-        self.argumans=arg
-        self.type = type
+    def __init__(self, data : pd.DataFrame = None) -> None :
+        self.data=data
 
-    def get(self):
+    
+    def median(self) -> dict:
+        """pozitif medyan ve negatif medyanı döndürür.
+        """
         pass
 
-    def median():
+    def percentile(self, percentile_value : float) -> dict:
         pass
 
-    def percentile(self):
-        print(self.argumans[0])
-
-
-class Condition:
-
-    def __init__(self,condition_type=None, *arg ,data=None):
-        self.condition_type = Condition_Types(condition_type,arg)
-
-
-    def implement(self):
+    def other_method(self):
         pass
+
+
+class Condition():
+    """Veriyi çeşitli koşulları baz alarak yeniden düzenlemek isteyebilirsiniz.
+    """
+
+    def __init__(self, data : pd.DataFrame = None) :
+        self.condition_type = Condition_Types(data) 
+        self._condition_value = None
+        
+    def set_type(self, method=None, params=None) -> None:
+        if method == 'median':
+            self._condition_value = self.condition_type.median()
+        elif method == 'percentile':
+            self._condition_value= self.condition_type.percentile(percentile_value = params)
+        else:
+            pass
+    
+    def implement(self) -> pd.DataFrame:
+        """ _condition_value'e göre dalgaları yeniden düzenler
+        """
+        pass
+
+
+# # Class'ın Uygulanması
+
+
+kosul = Condition('data')
+
+kosul.set_type(method='median')
+
+result = kosul.implement()
