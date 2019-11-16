bb27c4919b2f35db39a13d325f2c387bec808cd9
birdalugureren <birdalugureren@gmail.com>
2019-11-16 15:08:15 +0300

    two scenario

--- src/_condition.py
+++ src/_condition.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-
+#SENARYO 1
+#--------------------------------------------------------------------------------------------------
 # # Taslak
 class Condition_Types:
 
@@ -44,9 +45,47 @@ class Condition():
 
 # # Class'ın Uygulanması
 
-
 kosul = Condition('data')
-
 kosul.set_type(method='median')
-
 result = kosul.implement()
+# ---------------------------------------------------------------------------------------------------
+# SENARYO 2
+class Condition_Types:
+
+    def __init__(self, data : pd.DataFrame = None) -> None :
+        self.data=data
+    
+    def median(self) -> dict:
+        """pozitif medyan ve negatif medyanı döndürür.
+        """
+        pass
+
+    def percentile(self, percentile_value : float) -> dict:
+        pass
+
+    def other_method(self):
+        pass
+
+class Condition():
+    """Veriyi çeşitli koşulları baz alarak yeniden düzenlemek isteyebilirsiniz.
+    """
+
+    def __init__(self, data : pd.DataFrame = None) :
+        self._condition_values = None
+        
+    def set_values(self, values : dict) -> None:
+        self._condition_values = values
+        return self
+    
+    def implement(self) -> pd.DataFrame:
+        """ _condition_value'e göre dalgaları yeniden düzenler
+        """
+        pass
+
+
+## Class'ın Uygulanması
+
+
+con_values = Condition_Types('data').median()
+con= Condition('data').set_values(con_values)
+con.implement()
